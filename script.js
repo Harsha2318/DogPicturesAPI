512282f898cc3d46c7bdb9a1daf33c98c4da3626
Harsha P <113809648+Harsha2318@users.noreply.github.com>
2024-10-10 17:28:38 +0530

    Code is ready!!

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random";
 
+btn.addEventListener("click", async () => {
+    let link = await getimage();
+    let img = document.createElement("img");
+    img.setAttribute("src", link);
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("result").appendChild(img);
+    console.log(link);
+});
+
+async function getimage() {
+    try {
+        let res = await axios.get(url2);
+        return res.data.message;
+    } catch (e) {
+        console.log("error-", e);
+        return "/";
+    }
+}
